8ae70e5ecc2aef0f934d1e75e0721731d5e6af06
Thanh <quocthanh2929@gmail.com>
2023-07-16 22:32:36 +0700

    init source

---
+++ source/src/main/QuanLyCuaHangAlldy.java
@@ -0,0 +1,12 @@
+package main;
+
+import view.LoginView;
+
+public class QuanLyCuaHangAlldy {
+
+	public static void main(String[] args) {
+		LoginView dn = new LoginView();
+		dn.setVisible(true);
+	}
+
+}
---
+++ source/src/utils/Config.java
@@ -0,0 +1,8 @@
+package utils;
+
+public class Config {
+	public static String DB_URL = "jdbc:sqlserver://localhost:1433 ;databasename=QuanLyCuaHangAlldy";
+	public static String USER = "sa";
+	public static String PASSWORD = "sapassword";
+	
+}
