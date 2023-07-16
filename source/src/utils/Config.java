e3db9b2135bdfd39bf30097c40f28fa0062b5de2
Thanh <quocthanh2929@gmail.com>
2023-07-16 22:43:28 +0700

    update package

--- source/src/utils/Config.java
+++
@@ -1,8 +0,0 @@
-package utils;
-
-public class Config {
-	public static String DB_URL = "jdbc:sqlserver://localhost:1433 ;databasename=QuanLyCuaHangAlldy";
-	public static String USER = "sa";
-	public static String PASSWORD = "sapassword";
-	
-}
